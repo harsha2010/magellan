c1e0a1a562c273f451cd58009223c4a0372119d3
Christos <xr_xarmatzis@outlook.com>
2017-09-19 21:14:59 +0300

    added PolyLine tests

---
+++ src/test/scala/magellan/PolyLineSuite.scala
@@ -0,0 +1,104 @@
+/**
+ * Copyright 2015 Ram Sriharsha
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package magellan
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.spark.sql.types._
+import org.scalatest.FunSuite
+
+class PolyLineSuite extends FunSuite with TestSparkContext {
+
+  test("bounding box") {
+    val polyline = PolyLine(Array(0),Array(Point(0.0, 1.0), Point(1.0, 0.5)))
+    val BoundingBox(xmin, ymin, xmax, ymax) = polyline.getboundingBox
+    assert(xmin === 0.0)
+    assert(ymin === 0.5)
+    assert(xmax === 1.0)
+    assert(ymax === 1.0)
+  }
+
+  test("bounding box of multiLine") {
+    val polyline = PolyLine(Array(0,2),Array(Point(1.0, 1.0), Point(2.0, 2.0), Point(3.0, 3.0), Point(4.0, 4.0)))
+    val BoundingBox(xmin, ymin, xmax, ymax) = polyline.getboundingBox
+    assert(xmin === 1.0)
+    assert(ymin === 1.0)
+    assert(xmax === 4.0)
+    assert(ymax === 4.0)
+  }
+
+
+  test("polyline intersects line") {
+    val x = PolyLine(Array(0),Array(Point(0.0, 0.0), Point(1.0, 1.0)))
+    val y = Line(Point(1.0, 0.0), Point(0.0, 0.1))
+    val z = Line(Point(0.5, 0.0), Point(1.0, 0.5))
+    val w = Line(Point(1.0, -1.0), Point(-1.0, -1.0))
+    val l = Line(Point(0.0, -1.0), Point(0.0, -1.0))
+    val t = Line(Point(0.5, 0.5), Point(0.5, 0.0))
+    val u = Line(Point(1.0, 1.0), Point(2.0, 2.0))
+    val s = Line(Point(1.0, 1.0), Point(1.0, -1.0))
+    assert(x.intersects(y))
+    assert(!x.intersects(z))
+    assert(w.intersects(l))
+    assert(x.intersects(t))
+    assert(u.intersects(s))
+  }
+
+  test("polyline intersects polygon") {
+    val x = PolyLine(Array(0),Array(Point(0.0, 0.0), Point(1.0, 1.0)))
+    val w = Polygon(Array(0), Array(Point(1.0, 1.0), Point(1.0, -1.0),
+      Point(-1.0, -1.0), Point(-1.0, 1.0), Point(1.0, 1.0)))
+    val l = Polygon(Array(0), Array(Point(3.0, 3.0), Point(3.0, 2.0),
+      Point(2.0, 2.0), Point(2.0, 3.0), Point(3.0, 3.0)))
+    val t = Polygon(Array(0), Array(Point(3.0, 3.0), Point(3.0, -3.0),
+      Point(-3.0, -3.0), Point(-3.0, 3.0), Point(3.0, 3.0)))
+    assert(x.intersects(w))
+    assert(!x.intersects(l))
+    assert(x.intersects(t))
+  }
+
+
+
+  test("serialization") {
+    val polylineUDT = new PolyLineUDT
+    val polyline = PolyLine(Array(0), Array(Point(0.0, 1.0), Point(1.0, 0.5)))
+    val BoundingBox(xmin, ymin, xmax, ymax) = polyline.getboundingBox
+    val row = polylineUDT.serialize(polyline)
+    assert(row.getInt(0) === polyline.getType())
+    assert(row.getDouble(1) === xmin)
+    assert(row.getDouble(2) === ymin)
+    assert(row.getDouble(3) === xmax)
+    assert(row.getDouble(4) === ymax)
+    val serializedLine = polylineUDT.deserialize(row)
+    assert(polyline.equals(serializedLine))
+  }
+
+  test("contains line") {
+    val x = Line(Point(0.0, 0.0), Point(1.0, 1.0))
+    val y = Line(Point(0.0, 0.0), Point(0.5, 0.5))
+    val z = Line(Point(0.0, 0.0), Point(1.5, 1.5))
+    val w = Line(Point(0.5, 0.5), Point(0.0, 0.0))
+    val u = Line(Point(0.0, 0.0), Point(0.0, 1.0))
+    assert(x.contains(y))
+    assert(!x.contains(z))
+    assert(x.contains(w))
+    assert(!x.contains(u))
+  }
+
+  test("contains point") {
+    val y = PolyLine(Array(0), Array(Point(0.5, 0.0), Point(0.0, 0.5)))
+    assert(y.contains(Point(0.5, 0.0)))
+  }
+}
